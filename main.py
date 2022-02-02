17672eb0910dea29f75d1cee7c5d25f56fd3a8a2
vitdias <vitor.azambuja.90@gmail.com>
2022-02-02 00:59:50 -0300

    Interface Evolution
    + Starter Screen
    + Animation buttons
    + few colors

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 # Import libraries
+import time
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.properties import ObjectProperty, StringProperty
 from kivymd.app import MDApp
+from kivy.uix.image import Image
 import hashlib
 import sqlite3
 
-#Tela de Menu
-class MenuScreen(Screen):
+#Tela de Login
+class LoginScreen(Screen):
+    pass
+
+class StartScreen(Screen):
     pass
 
 # Tela de App
@@ -27,23 +32,38 @@ class ChangePwScreen(Screen):
 # Tela de cadastro
 class RegisterAccScreen(Screen):
     def register_acc(self):
-        pass
+        user=ObjectProperty(None)
+        email=ObjectProperty(None)
+        conf_email=ObjectProperty(None)
+        password=ObjectProperty(None)
+        conf_password=ObjectProperty(None)
+        # lbl_out=ObjectProperty(None)
+        return print(user)
+        
+    
+
+# Screen Manager
+sm = ScreenManager()
+sm.add_widget(StartScreen(name="start"))
+sm.add_widget(LoginScreen(name="menu"))
+sm.add_widget(RegisterAccScreen(name="register"))
 
 # Tela de login do usuário
-class LoginScreen(MDApp):
+class NinApp(MDApp):
+    
     def build(self):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "BlueGray"
+        # self.theme_cls.primary_hue = "50" 
         self.title = "Login on NinaData"
         self.db = sqlite3.connect("acessos.db")
         self.db.execute("CREATE TABLE IF NOT EXISTS tb_usuarios(id INTEGER PRIMARY KEY AUTOINCREMENT, userLogin TEXT, userEmail TEXT, userPW TEXT, userSecretQuestion TEXT, userSecretAnswer TEXT)")
+        self.cursor = self.db.cursor()
         Window.size = (300, 600)
-        return Builder.load_file('gui.kv')
-    
-    def login(self):
-        self.root.ids.welcome_label.text = f'Logged as {self.root.ids.user.text}'
+        Window.clearcolor = (255,191,168,1)
         
-    def register(self):
-        pass
+        screen = Builder.load_file('gui.kv')
+        return screen
     
-LoginScreen().run()
+        
+NinApp().run()
